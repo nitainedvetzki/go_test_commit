24c9d454fb007c2183ff56096af288ae6f05b56b
nitainedvetzki <106998867+nitainedvetzki@users.noreply.github.com>
2023-08-01 17:12:28 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -5,77 +5,43 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/minio/minio-go/v7"
-	"github.com/minio/minio-go/v7/pkg/credentials"
+	crossplaneClient "github.com/crossplane/crossplane-runtime/pkg/client"
+	"github.com/crossplane/crossplane/apis/stacks/v1alpha1"
+	"k8s.io/client-go/rest"
 )
 
 func main() {
-	// MinIO server endpoint, access key, and secret key
-	endpoint := "your-minio-endpoint"       // Replace with your MinIO server endpoint
-	accessKeyID := "your-access-key-id"     // Replace with your access key ID
-	secretAccessKey := "your-secret-key"    // Replace with your secret access key
-	useSSL := false                         // Set to true if your MinIO server uses SSL
-
-	// Initialize MinIO client
-	minioClient, err := minio.New(endpoint, &minio.Options{
-		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
-		Secure: useSSL,
-	})
+	// Create a Kubernetes REST client configuration
+	config, err := rest.InClusterConfig()
 	if err != nil {
-		log.Fatalf("Error initializing MinIO client: %v", err)
+		log.Fatalf("Error creating in-cluster config: %v", err)
 	}
 
-	// Create a new bucket (optional)
-	bucketName := "your-bucket-name" // Replace with the desired bucket name
-	location := "your-location"     // Replace with your desired bucket location
-	ctx := context.Background()
-
-	err = minioClient.MakeBucket(ctx, bucketName, minio.MakeBucketOptions{Region: location})
+	// Create a Crossplane client
+	crossplaneClient, err := crossplaneClient.NewClient(config, 0)
 	if err != nil {
-		exists, errBucketExists := minioClient.BucketExists(ctx, bucketName)
-		if errBucketExists == nil && exists {
-			log.Printf("Bucket '%s' already exists.", bucketName)
-		} else {
-			log.Fatalf("Error creating bucket: %v", err)
-		}
-	} else {
-		log.Printf("Bucket '%s' created successfully in location '%s'.", bucketName, location)
+		log.Fatalf("Error creating Crossplane client: %v", err)
 	}
 
-	// Upload an object to the bucket
-	objectName := "your-object-name"         // Replace with the desired object name
-	filePath := "path/to/your/local/file"    // Replace with the local file path you want to upload
-
-	// Open the local file
-	file, err := os.Open(filePath)
-	if err != nil {
-		log.Fatalf("Error opening file: %v", err)
-	}
-	defer file.Close()
-
-	// Get the file stats to get its size
-	stat, err := file.Stat()
-	if err != nil {
-		log.Fatalf("Error getting file stats: %v", err)
-	}
-
-	// Set object options with content type and size
-	opts := minio.PutObjectOptions{
-		ContentType: "application/octet-stream",
-		UserMetadata: map[string]string{
-			"my-key": "my-value",
+	// Create a MySQL resource claim object
+	mysqlClaim := &v1alpha1.MySQLInstanceClaim{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "example-mysql-claim",
+			Namespace: "default",
 		},
-		// The size can be determined from the file's stat
-		// The content size must be specified for objects > 5MB to enable multipart upload.
-		ContentLength: stat.Size(),
+		Spec: v1alpha1.MySQLInstanceClaimSpec{
+			ClassSelector: &v1alpha1.ClassSelector{
+				Name: "standard-mysql-instance", // Replace with the name of the MySQL class you want to use
+			},
+		},
 	}
 
-	// Upload the object with the specified options
-	n, err := minioClient.PutObject(ctx, bucketName, objectName, file, opts)
+	// Create or update the MySQL resource claim
+	err = crossplaneClient.Create(context.Background(), mysqlClaim)
 	if err != nil {
-		log.Fatalf("Error uploading object: %v", err)
+		log.Fatalf("Error creating MySQL resource claim: %v", err)
 	}
 
-	log.Printf("Uploaded %s of size %d successfully.", objectName, n)
+	fmt.Println("MySQL resource claim created successfully.")
 }
 
